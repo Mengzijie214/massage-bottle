10859a826d0ad27bbcd37eac39e7a683d9c90219
mengzijie214 <3540722620@qq.com>
2025-03-17 23:17:48 +0800

    更新了数据库结构文件以及正式名：解忧漂流瓶-治愈您的心灵

--- message_bottle.sql
+++ message_bottle.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80041 (8.0.41)
  File Encoding         : 65001
 
- Date: 17/03/2025 18:40:11
+ Date: 17/03/2025 23:06:27
 */
 
 SET NAMES utf8mb4;
@@ -32,12 +32,6 @@ CREATE TABLE `bottles`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of bottles
--- ----------------------------
-INSERT INTO `bottles` VALUES (1, 2, '苦呀西，写代码好难QAQ，如果在给我一次机会，我一定要找几个苦力帮我干活，而不是我一只猿QAQ', '2025-03-15 15:22:04');
-INSERT INTO `bottles` VALUES (2, 6, '测试测试', '2025-03-17 18:17:33');
-
--- ----------------------------
 -- Table structure for replies
 -- ----------------------------
 DROP TABLE IF EXISTS `replies`;
@@ -55,11 +49,6 @@ CREATE TABLE `replies`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of replies
--- ----------------------------
-INSERT INTO `replies` VALUES (1, 1, 3, '抱抱你哟，辛苦啦！', '2025-03-15 16:47:12');
-
--- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
@@ -70,12 +59,5 @@ CREATE TABLE `users`  (
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `username`(`username` ASC) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES (2, 'Ayanamy', '$5$rounds=535000$uwkIei9YTgP3Qpbe$mjbZ0QV0LdBXG22jRl9kON213CkUYEM.ODhQePc7qU5');
-INSERT INTO `users` VALUES (3, 'tojyiku', '$5$rounds=535000$yDFcRtweqbFNJSX7$orKTv7BVxIFRgunj6mapu8HwWdssd6gjHsxIAK5pAc4');
-INSERT INTO `users` VALUES (6, '一只小凌波', '$5$rounds=535000$.yvmR1t20q1uELiM$VT4KTj1uXXY.c3rlp/hw2Xy14gUKerOO0jIwHPOpJY1');
 
 SET FOREIGN_KEY_CHECKS = 1;
